a168cf7121431b1cacf49d5285849572d45f4e47
pab-h <dev.pab.2020@gmail.com>
2026-07-21 20:23:14 -0300

    feat: ensure models used on the project in ollama container

---
+++ app/ollama_setup.py
@@ -0,0 +1,25 @@
+from ollama  import AsyncClient
+from app.env import settings
+
+async def ensure_models_exists():
+    
+    models_required = [
+        settings.embedding_model
+    ]
+
+    client = AsyncClient(host = settings.ollama_url)
+    
+    response = await client.list()
+    
+    installed_models = [m.model for m in response.models] 
+
+    for model in models_required:
+
+        model_exists = any(installed.startswith(model) for installed in installed_models)
+        
+        if not model_exists:
+            print(f"Model '{model}' not found. Downloading...")
+            await client.pull(model = model)
+        else:
+            print(f"Model'{model}' already exists.")
+    
