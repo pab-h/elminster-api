42610d16b35df0d85f33e1137815ca6c759ff9cc
pab-h <dev.pab.2020@gmail.com>
2026-07-25 15:05:53 -0300

    cleanup: the tables will be created by Alembic

--- app/api/main.py
+++ app/api/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 
-from app.database     import create_db_and_tables
 from app.ollama_setup import ensure_models_exists
 from app.api.routes   import digest
 from app.api.routes   import query
@@ -10,7 +9,6 @@ from contextlib import asynccontextmanager
 @asynccontextmanager
 async def lifespan(app: FastAPI):
 
-    create_db_and_tables()
     await ensure_models_exists()
 
     yield
