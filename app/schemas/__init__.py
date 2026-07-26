50faf0adb02acf00151d09ce4dcdab07b325f3a8
pab-h <dev.pab.2020@gmail.com>
2026-07-26 09:43:40 -0300

    feat: user schemas

---
+++ app/schemas/__init__.py
@@ -0,0 +1,3 @@
+from app.schemas.users import UserCreate
+from app.schemas.users import UserRead
+from app.schemas.users import UserUpdate
---
+++ app/schemas/users.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from uuid     import UUID
+
+from pydantic import BaseModel
+from pydantic import EmailStr
+
+class UserBase(BaseModel):
+    name:  str
+    email: EmailStr
+
+class UserCreate(UserBase):
+    password: str
+
+class UserRead(UserBase):
+    id:         UUID
+    created_at: datetime
+
+class UserUpdate(BaseModel):
+    name:     str | None = None
+    email:    str | None = None
+    password: str | None = None
