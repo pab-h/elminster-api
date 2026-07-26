6ed92315fc80812e42540a8a089c2b01d0225c67
pab-h <dev.pab.2020@gmail.com>
2026-07-26 09:42:59 -0300

    feat: email validation with pydantic

--- app/models/users.py
+++ app/models/users.py
@@ -7,10 +7,9 @@ from datetime import timezone
 from uuid import UUID
 from uuid import uuid4
 
-from app.env import settings
-
 class User(SQLModel, table = True):
     id:         UUID     = Field(default_factory = uuid4, primary_key = True)
+    email:      str      = Field(unique = True)
     name:       str
     password:   str
     created_at: datetime = Field(default_factory = lambda: datetime.now(timezone.utc))
