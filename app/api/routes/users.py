2e22d37b961208cecf8c14e339af3d6e272ee8b5
pab-h <dev.pab.2020@gmail.com>
2026-07-27 20:51:23 -0300

    feat: user services

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -1,22 +1,21 @@
 from uuid import UUID
 
 from fastapi import APIRouter
-from fastapi import status
 from fastapi import Depends
-from fastapi import HTTPException
+from fastapi import status
 
 from sqlmodel import Session
-from sqlmodel import select
 
-from app.models   import User
 from app.database import get_db_session
 
 from app.schemas import UserCreate
 from app.schemas import UserRead
 from app.schemas import UserUpdate
 
+from app.services import users as user_service
+
 router = APIRouter(
-    tags   = ["Users"],
+    tags   = ["Users"], 
     prefix = "/users"
 )
 
@@ -25,38 +24,31 @@ async def create_user(
     user_data: UserCreate,
     session:   Session = Depends(get_db_session)
 ) -> UserRead:
-
-    user = User(
-        name     = user_data.name,
-        email    = user_data.email,
-        password = user_data.name,
-    )
     
-    session.add(user)
-    session.commit()
-    session.refresh(user)
-
-    return user
+    return await user_service.create_user(
+        user_data = user_data, 
+        session   = session
+    )
 
 @router.get("/{id}", status_code = status.HTTP_200_OK)
 async def find_user(
     id:      UUID,
     session: Session = Depends(get_db_session)
 ) -> UserRead:
-    user = session.get(User, id)
-    if not user:
-        raise HTTPException(
-            status_code = status.HTTP_404_NOT_FOUND, 
-            detail      = "User not found"
-        )
-    return user    
+    
+    return await user_service.find_user(
+        id      = id, 
+        session = session
+    )
 
 @router.get("/", status_code = status.HTTP_200_OK)
 async def find_all_user(
     session: Session = Depends(get_db_session)
 ) -> list[UserRead]:
-    users = session.exec(select(User)).all()
-    return users
+    
+    return await user_service.find_all_user(
+        session = session
+    )
 
 @router.put("/{id}", status_code = status.HTTP_200_OK)
 async def update_user(
@@ -64,38 +56,23 @@ async def update_user(
     user_data: UserUpdate,
     session:   Session = Depends(get_db_session)
 ) -> UserRead:
-    user = session.get(User, id)
-    if not user:
-        raise HTTPException(
-            status_code = status.HTTP_404_NOT_FOUND, 
-            detail      = "User not found"
-        )
     
-    user_dict = user_data.model_dump(exclude_unset=True)
-    for key, value in user_dict.items():
-        if key != "id": 
-            setattr(user, key, value)
-            
-    session.add(user)
-    session.commit()
-    session.refresh(user)
-    
-    return user    
+    return await user_service.update_user(
+        id        = id, 
+        user_data = user_data, 
+        session   = session
+    )
 
 @router.delete("/{id}", status_code = status.HTTP_200_OK)
 async def delete_user(
     id:      UUID,
     session: Session = Depends(get_db_session)
 ):
-    user = session.get(User, id)
-    if not user:
-        raise HTTPException(
-            status_code = status.HTTP_404_NOT_FOUND, 
-            detail      = "User not found"
-        )
-        
-    session.delete(user)
-    session.commit()
+    
+    await user_service.delete_user(
+        id      = id, 
+        session = session
+    )
 
     return {
         "message": "User successfully removed"
